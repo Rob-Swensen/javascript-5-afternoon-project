aa4e1884e7c80dd0315d128a08fb258eb2afcd58
Rob-Swensen <rg.swensen801@gmail.com>
2020-02-24 12:35:27 -0700

    First Commit

--- classes.js
+++ classes.js
@@ -30,6 +30,17 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -48,7 +59,18 @@
 */
 
 //Code Here
-
+class Manager extends Employee {
+    constructor(first_name, last_name, email, age, reports){
+        super(first_name, last_name, email, age, reports)
+        this.reports = []
+    }
+    hire(employee){
+      this.reports.push(employee)
+    }
+    fire(index){
+      this.reports.splice(index,1)
+    }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -72,6 +94,31 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    super(first_name, last_name, email, age, reports, title, bonus)
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  } 
+  hired(){
+    if(this.reports.length === 0){
+      this.title = 'Not a manager'
+    } else if (this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = 'Barely Manager'
+    } else if (this.reports.length >= 4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager'
+    } else if (this.reports.length >= 11 && this.reports.length <= 50){
+      this.title = 'Manager'
+    } else if (this.reports.length >= 51 && this.reports.length <= 100){
+      this.title = 'Manager Plus'
+    } else {
+      this.title = 'Bestest Manager'
+    }
+  }
+  fired(){
+    this.bonus = num * 100
+  }
+}
 
 
 
@@ -99,5 +146,26 @@
 */
 
 //Code Here
-
+class Machine {
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    // if(this.widgets_made_count % 50 === 0){
+    //   this.wear_and_tear_count += 1
+    // }
+  },
+  fixMachine(){
+    this.needs_reboot = true
+  },
+  reboot(){
+    return function(){
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false
+    }
+  }
+}
 
